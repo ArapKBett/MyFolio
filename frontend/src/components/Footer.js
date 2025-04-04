de5f73397938ee2c1d3fe305018713a4951ed247
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-04 06:32:16 -0400

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="py-6 bg-gray-800 text-center text-gray-400">
+      <p>© 2025 Kipkorir Bett. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
